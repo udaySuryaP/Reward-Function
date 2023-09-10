690a6d86baa653b92e004a4320e23bc2ddcdcfcd
Uday Surya Prasanth <133751051+udaySuryaP@users.noreply.github.com>
2023-09-10 20:28:12 +0530

    Update reward function.py
    Modeel-Sept

--- model/reward function.py
+++ model/reward function.py
@@ -3,8 +3,8 @@ import math
 def reward_function(params):
     MAX_REWARD = 1e3
     MIN_REWARD = 1e-3
-    DIRECTION_THRESHOLD = 15.0
-    ABS_STEERING_THRESHOLD = 25
+    DIRECTION_THRESHOLD = 20.0  # Increased threshold for steering direction
+    ABS_STEERING_THRESHOLD = 30  # Increased threshold for steering angle
 
     on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
@@ -16,16 +16,16 @@ def reward_function(params):
     heading = params['heading']
 
     # Negative exponential penalty based on distance from center
-    reward = math.exp(-5 * distance_from_center)
+    reward = math.exp(-3 * distance_from_center)  # Reduced penalty coefficient
 
     # Reward for staying on track
     if not on_track:
         return float(MIN_REWARD)
 
     # Calculate distance from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.4 * track_width
+    marker_1 = 0.2 * track_width  # Adjusted markers
+    marker_2 = 0.5 * track_width
+    marker_3 = 0.8 * track_width
 
     # Give higher reward if the car is closer to the center line
     if distance_from_center <= marker_1:
@@ -34,12 +34,10 @@ def reward_function(params):
         reward *= 0.8
     elif distance_from_center <= marker_3:
         reward += 0.2
-    else:
-        return float(MIN_REWARD)  # Likely crashed/off-track
 
     # Reward for driving straight and fast
-    if abs(steering) < 0.05 and speed > 4:
-        reward *= 1.2
+    if abs(steering) < 5.0 and speed > 4.0:  # Adjusted steering and speed conditions
+        reward *= 1.5
 
     # Calculate the direction of the center line based on the closest waypoints
     next_point = waypoints[closest_waypoints[1]]
@@ -50,16 +48,17 @@ def reward_function(params):
 
     # Penalize if the car is not heading in the right direction
     if direction_diff > DIRECTION_THRESHOLD:
-        reward *= 0.6
+        reward *= 0.7  # Slightly less penalty
 
     # Penalize if the car is steering too much
     if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.7
+        reward *= 0.8  # Slightly less penalty
 
     # Adjust throttle based on steering angle
-    throttle_adjustment = 0.25
-    target_speed = 3.0  # Adjust the target speed as desired
+    throttle_adjustment = 0.3  # Reduced throttle adjustment
+    target_speed = 7.0  # Increased target speed
     if speed > target_speed - (throttle_adjustment * abs(steering)):
-        reward *= 0.8
+        reward *= 0.9  # Slightly less penalty
 
-    return float(reward)
+    # Cap the reward to avoid excessive values
+    return max(MIN_REWARD, min(MAX_REWARD, float(reward)))
